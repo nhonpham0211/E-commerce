1cb8d0c2dd509368dd4f5bfc6f9e303595ccfe7e
NhonPham <nhonpham0211@gmail.com>
2021-02-18 17:33:32 +0700

    project final

---
+++ src/components/mainpages/detailProduct/Detailproduct.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { GlobalState } from "../../../GlobalState";
+import ProductItem from "../utils/productItem/ProductItem";
+
+export default function DetailProduct() {
+  const params = useParams();
+  const state = useContext(GlobalState);
+  const [products] = state.productsAPI.products;
+  const addCart = state.userAPI.addCart;
+  const [detailProduct, setDetailProduct] = useState([]);
+
+  useEffect(() => {
+    if (params.id) {
+      products.forEach((product) => {
+        if (product._id === params.id) setDetailProduct(product);
+      });
+    }
+  }, [params.id, products]);
+
+  if (detailProduct.length === 0) return null;
+  const {
+    title,
+    product_id,
+    price,
+    description,
+    content,
+    sold,
+    category,
+  } = detailProduct;
+  return (
+    <>
+      <div className="detail">
+        <img src={detailProduct.images.url} alt="" />
+        <div className="box-detail">
+          <div className="row">
+            <h2>{title}</h2>
+            <h6>#id: {product_id}</h6>
+          </div>
+          <span>${price}</span>
+          <p>{description}</p>
+          <p>{content}</p>
+          <p>Sold: {sold}</p>
+          <Link
+            to="/cart"
+            className="cart"
+            onClick={() => addCart(detailProduct)}
+          >
+            Buy Now
+          </Link>
+        </div>
+      </div>
+      <div>
+        <h2>Related Products</h2>
+        <div className="products">
+          {products.map((product) => {
+            return product.category === category ? (
+              <ProductItem key={product._id} product={product} />
+            ) : null;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
