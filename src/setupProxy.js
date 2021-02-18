1e087e7e72f3a7047ed60084d41d41f8e97a8718
NhonPham <nhonpham0211@gmail.com>
2021-02-18 22:46:34 +0700

    fix proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://ecommerce-2021.herokuapp.com/",
+      changeOrigin: true,
+    })
+  );
+};
